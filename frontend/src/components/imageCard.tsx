d1560d6f542653df29616fce2c4af8f721330dcd
MathieuBusiness <mathieucomeau.business@gmail.com>
2026-02-03 06:13:04 -0500

    45: refactor imageCard

--- frontend/src/components/imageCard.tsx
+++ frontend/src/components/imageCard.tsx
@@ -7,26 +7,18 @@ type Props = {
 const imageCard = ({ image }: Props) => {
     return (
         <div className="border rounded overflow-hidden shadow-sm">
-            {/* Photo */}
             <img
                 src={image.imageUrl}
                 alt={image.id}
                 className="w-full object-cover"
             />
-
-            {/* Info */}
             <div className="p-3 space-y-1 text-sm">
-                {/* Date */}
                 <div className="text-gray-500 text-xs">
                     {new Date(image.createdAt).toLocaleDateString()}
                 </div>
-
-                {/* Description */}
                 {image.description && (
                     <p className="text-sm">{image.description}</p>
                 )}
-
-                {/* Hashtags */}
                 {image.hashtags.length > 0 && (
                     <div className="flex flex-wrap gap-2">
                         {image.hashtags.map((h) => (
@@ -36,8 +28,6 @@ const imageCard = ({ image }: Props) => {
                         ))}
                     </div>
                 )}
-
-                {/* Mentions */}
                 {image.mentionUserIds.length > 0 && (
                     <div className="flex gap-2">
                         {image.mentionUserIds.map((id) => (
