e624a5bcf92cbed71ac49b0fb32466a341587a55
s.comeau <sandrine.comeau.1@ulaval.ca>
2026-02-07 15:23:53 -0500

    reformat nav bar

--- frontend/src/components/navbar.tsx
+++ frontend/src/components/navbar.tsx
@@ -1,33 +1,43 @@
 import { NavLink } from 'react-router';
-import UgramLogo from '../assets/ugramOutline.tsx';
+import { MagnifyingGlassCircleIcon } from '@heroicons/react/24/outline';
+import { HomeIcon } from '@heroicons/react/24/outline';
+import { UserCircleIcon } from '@heroicons/react/24/outline';
 import UgramLettering from '../assets/ugramLettering.tsx';
 
 export default function Navbar() {
     return (
         <div className="bg-gradient-to-r from-primary to-secondary pb-[2px] rounded sticky top-0 w-full ">
             <nav className="flex justify-between px-3 pt-3 pb-2 md:pb-1 bg-dark">
-                <NavLink
-                    to="/"
-                    className="hover:text-accent flex items-center flex-col"
-                >
-                    <UgramLogo className="w-6 h-6 block md:hidden" />
-                    <p className="text-sm block md:hidden">Home</p>
-                    <UgramLettering className="hidden md:block"></UgramLettering>
-                </NavLink>
-                <NavLink
-                    to="/Search"
-                    className="hover:text-accent flex items-center flex-col md:flex-row"
-                >
-                    <UgramLogo className="w-6 h-6 mx-1" />
-                    <p className="text-sm md:text-base">Search</p>
-                </NavLink>
-                <NavLink
-                    to="/Profile"
-                    className="hover:text-accent flex items-center flex-col md:flex-row"
-                >
-                    <UgramLogo className="w-6 h-6 mx-1" />
-                    <p className="text-sm md:text-base">Profile</p>
-                </NavLink>
+                <UgramLettering className="hidden md:block"></UgramLettering>
+                <div className="flex justify-between w-full md:w-fit">
+                    <NavLink
+                        to="/"
+                        className="group hover:text-accent flex items-center flex-col md:flex-row mx-1"
+                    >
+                        <HomeIcon className="size-6 mx-1" />
+                        <p className="text-sm md:text-base md:hidden group-hover:block">
+                            Home
+                        </p>
+                    </NavLink>
+                    <NavLink
+                        to="/Search"
+                        className="group hover:text-accent flex items-center flex-col md:flex-row mx-1"
+                    >
+                        <MagnifyingGlassCircleIcon className="size-6 mx-1" />
+                        <p className="text-sm md:text-base md:hidden group-hover:block">
+                            Search
+                        </p>
+                    </NavLink>
+                    <NavLink
+                        to="/Profile"
+                        className="group hover:text-accent flex items-center flex-col md:flex-row mx-1"
+                    >
+                        <UserCircleIcon className="size-6 mx-1" />
+                        <p className="text-sm md:text-base md:hidden group-hover:block">
+                            Profile
+                        </p>
+                    </NavLink>
+                </div>
             </nav>
         </div>
     );
